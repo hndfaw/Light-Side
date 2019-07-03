635d7047d86819d2b8dc8bdbb65dced8964933a5
Anneke McGrady <annekemcgrady@gmail.com>
2019-07-03 16:53:23 -0600

    Delete extra cards we don't need anymore

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Card.css';
 
-const PeopleCard = ({ item }) => {
+const Card = ({ item }) => {
   return (
     <section className="card">
       <h2 className="card-header">{item.name}</h2>
@@ -16,4 +16,34 @@ const PeopleCard = ({ item }) => {
   )
 }
 
-export default PeopleCard;
+// const PlanetCard = ({ planet }) => {
+//   return (
+//     <section className="card">
+//       <h2 className="card-header">{planet.name}</h2>
+//       <div className="card-body">
+//         <p>Terrain: {planet.terrain}</p>
+//         <p>Diameter: {planet.diameter}</p>
+//         <p>Population: {planet.population}</p>
+//       </div>
+//       <footer className="card-footer"></footer>
+//     </section>
+//   )
+// }
+
+// const VehicleCard = ({ vehicle }) => {
+//   return (
+//     <section className="card">
+//       <h2 className="card-header">{person.name}</h2>
+//       <div className="card-body">
+//         <p>Model: {vehicle.model}</p>
+//         <p>Class: {vehicle.vehicle_class}</p>
+//         <p>Number of Passengers: {vehicle.passengers}</p>
+//       </div>
+//       <footer className="card-footer"></footer>
+//     </section>
+//   )
+// }
+
+
+
+export default Card;
--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -6,14 +6,14 @@ import Card from '../Card/Card.js'
 
 const CardContainer = ({ data }) => {
 
-const peopleCards = data.map( item => {
+const Cards = data.map( item => {
  return <Card key={Date.now()} item={item} />
 
   })
 
   return (
     <main className="main">
-      { peopleCards }
+      { Cards }
     </main>
   )
 
