c566ca4b4cf21530cc050943a0c7f13e578c9318
Anneke McGrady <annekemcgrady@gmail.com>
2019-07-03 18:57:56 -0600

    Create Home component and make Aside scrolling work

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css'
+
+
+const Home = () => {
+
+  return (
+
+  <aside className="home">
+    <p>Welcome to LiteSite, please choose a category</p>
+  </aside>
+  )
+
+      
+}
+
+export default Home;
